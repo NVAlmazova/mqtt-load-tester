427a393c327d8a7f65dd19b95f2826f9e551c936
Natalya Almazova <m154907@edu.misis.ru>
2020-11-28 22:53:07 +0300

    fixed

--- mqtt-load-tester.py
+++ mqtt-load-tester.py
@@ -1,14 +1,38 @@
 import argparse
+import time, threading
+import string
+import random
+import paho.mqtt.client as mqtt
 
 # <HOST> <PORT> <TOPIC> <MESSAGE> <FREQUENCY>
 
-parser = argparse.ArgumentParser()
+WAIT_SECONDS = 1
 
+parser = argparse.ArgumentParser()
+3
 parser.add_argument('host', type=str)
-parser.add_argument('--port', default=1833, type=int)
+parser.add_argument('--port', default=1883, type=int)
 parser.add_argument('topic', type=str)
-parser.add_argument('--message', default='', type=str)
+parser.add_argument('--message_lenght', default=116, type=int, dest='ml')
 parser.add_argument('--frequency', default=10, type=int)
 
 args = parser.parse_args()
-print(args.accumulate(args.integers))
+
+
+def randstr(st=string.ascii_letters, N=args.ml):
+    return ''.join(random.choice(st) for _ in range(N))
+
+
+def send_mqtt():
+    print(randstr())
+    threading.Timer(args.frequency, send_mqtt).start()
+    return randstr()
+
+
+mqttc = mqtt.Client()
+mqttc.connect(args.host, args.port)
+mqttc.publish(args.topic, payload=send_mqtt())
+
+# print(randstr(N=3))
+
+# send_mqtt()
