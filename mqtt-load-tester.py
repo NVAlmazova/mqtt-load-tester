296524f64eb8f8a333e4acf5d91c37c017fab2d9
Natalya Almazova <m154907@edu.misis.ru>
2020-11-28 23:29:38 +0300

    Fixed

--- mqtt-load-tester.py
+++ mqtt-load-tester.py
@@ -6,14 +6,11 @@ import paho.mqtt.client as mqtt
 
 # <HOST> <PORT> <TOPIC> <MESSAGE> <FREQUENCY>
 
-WAIT_SECONDS = 1
-
 parser = argparse.ArgumentParser()
-3
 parser.add_argument('host', type=str)
 parser.add_argument('--port', default=1883, type=int)
 parser.add_argument('topic', type=str)
-parser.add_argument('--message_lenght', default=116, type=int, dest='ml')
+parser.add_argument('--message_length', default=116, type=int, dest='ml')
 parser.add_argument('--frequency', default=10, type=int)
 
 args = parser.parse_args()
@@ -25,14 +22,11 @@ def randstr(st=string.ascii_letters, N=args.ml):
 
 def send_mqtt():
     print(randstr())
-    threading.Timer(args.frequency, send_mqtt).start()
-    return randstr()
+    mqttc.publish(args.topic, payload=randstr())
+    threading.Timer(1 / args.frequency, send_mqtt).start()
 
 
 mqttc = mqtt.Client()
 mqttc.connect(args.host, args.port)
-mqttc.publish(args.topic, payload=send_mqtt())
 
-# print(randstr(N=3))
-
-# send_mqtt()
+send_mqtt()
